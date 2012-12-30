43ee1ff2b66c564b23fb696ce1c9f290f7b8c266
nokok <nokok.kz@gmail.com>
2012-12-30 23:15:58 +0900

    PostgreSQLの実装開始

--- 403BookManagementSystem/src/PostgreSQL.java
+++ 403BookManagementSystem/src/PostgreSQL.java
@@ -1,4 +1,52 @@
 
-public class PostgreSQL {
+public class PostgreSQL implements IDatabase{
 
+	@Override
+	public boolean addBook(String ISBN) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean rmBook(String ISBN) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean rmBookList(String[] books) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean bBook(String ISBN) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean rBook(String ISBN) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public String[] searchDB(String key) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public String[] listDB() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public String[] listStatus(int mode) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+	
 }
