8db4ff079ed2c715a53e03285648dca970456d54
nokok <nokok.kz@gmail.com>
2012-12-31 20:16:53 +0900

    XMLTextProc テキストの文字化け修正メソッド削除

--- 403BookManagementSystem/src/XMLTextProc.java
+++ 403BookManagementSystem/src/XMLTextProc.java
@@ -19,7 +19,6 @@ public class XMLTextProc {
 		int endIndex = xml.indexOf("</" + key + ">");
 		String result = xml
 				.substring((beginIndex + key.length() + 2), endIndex);
-		result = removeMissingChar(result);
 		return result;
 	}
 
@@ -29,31 +28,18 @@ public class XMLTextProc {
 		//System.out.println(xml.substring((beginIndex + key.length() + 2),endIndex));
 		String result = xml
 				.substring((beginIndex + key.length() + 2), endIndex);
-		result = removeMissingChar(result);
 		return result;
 	}
 
 	public String searchMultiValues(final String key) {
 		int start = 0;
-		String tmp = "";
+		String result = "";
 		Pattern p = Pattern.compile("<" + key + ">");
 		Matcher m = p.matcher(xml);
 		while (m.find(start)) {
 			start = m.end();
-			tmp += xml.substring(start, xml.indexOf("</", start)) + ",";
+			result += xml.substring(start, xml.indexOf("</", start)) + ",";
 		}
-		tmp = removeMissingChar(tmp);
-		return tmp;
-	}
-
-	private String removeMissingChar(String str) {
-		str = str.replace('~',' ');
-		str = str.replaceAll("\\?", "");
-		str = str.replaceAll("　", "");
-		str = str.replaceAll("　\\?", "");
-		if (str.endsWith(",")) {
-			str = str.substring(0, str.length() - 1);
-		}
-		return str;
+		return result;
 	}
 }
